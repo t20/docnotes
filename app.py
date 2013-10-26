8968d664311a43a02a6cda73bc26a6473981c6b0
Bharad <barbi.bruce@gmail.com>
2013-10-26 14:31:11 -0700

    stubs for various end points and empty templates

--- app.py
+++ app.py
@@ -1,14 +1,52 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
-    return "Index Page!"
+    return render_template('index.html')
 
-@app.route('/hello')
-def hello_world():
-    return "Hello World!"
+
+@app.route('/medications')
+def medications():
+    return render_template('medications.html')
+
+
+@app.route('/diet')
+def diet():
+    return render_template('diet.html')
+
+
+@app.route('/followups')
+def followups():
+    return render_template('followups.html')
+
+
+@app.route('/symptoms')
+def index():
+    return render_template('symptoms.html')
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+
+@app.route('/share')
+def share():
+    return render_template('share.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
 
 if __name__ == '__main__':
     app.debug = True
