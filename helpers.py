cf14c79f499c8a4b07efb4a6fcc63b46f84c3b2d
Bharad <barbi.bruce@gmail.com>
2013-10-27 11:23:21 -0700

    Added more api stuff

--- helpers.py
+++ helpers.py
@@ -2,5 +2,9 @@ import requests
 
 def process_data(data):
     # read data from input
+    # name = request.args.get('name', "John")
+    # admission = request.args.get('admission', "Heart Failure")
+    # medication = request.args.get('medication', [])
+    # appointments = request.args.get('appointments', "")
     # upload record to the database
     return
